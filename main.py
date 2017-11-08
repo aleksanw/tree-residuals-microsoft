6ea12e1b9f809674bf9efc94c10a87cfef6c973f
Aleksander Wasaznik <aleksander@wasaznik.com>
2017-11-08 17:03:39 +0100

    Working: NChain overfitting

--- main.py
+++ main.py
@@ -231,6 +231,7 @@ def test_policy(policy, env):
 def decay(initial, t):
     return initial/(1 + 0.04*t)
 
+
 class Policy_ConstantActionLoop:
     def __init__(self, actions):
         self.actions = itertools.cycle(actions)
@@ -239,32 +240,44 @@ class Policy_ConstantActionLoop:
         return self.actions.__next__()
 
 
-def runner():
+def run():
     env = gym.make('NChain-v0')
+    env.unwrapped.slip = 0  # nonslip env
     action_space = list(range(env.action_space.n))
 
-    initial_learning_rate = learning_rate = 0.15
-    initial_epsilon = epsilon = 0.4
-    episodes_per_update = 10
+    # Generate episodes by just repeatedly applying the same action
+    loops = [
+            [0],
+            [1],
+            [0,1],
+            [0,0,1],
+            [0,0,0,1],
+            [0,0,0,0,1],
+            ]
 
+    episodes = []
+    for loop in loops:
+        episodes.append(list(rollout(Policy_ConstantActionLoop(loop), env)))
+
+
+    # Attempt to overfit targets
+    # Hopefully the approximator will still generalize to states it has not seen
     q = Approximator_ResidualBoosting(action_space)
-    for learn_iteration in itertools.count():
-        learning_rate = decay(initial_learning_rate, learn_iteration)
-        epsilon = decay(initial_epsilon, learn_iteration)
-        policy = Policy_EpsilonGreedy(q, epsilon)
-        episodes = [list(rollout(policy, env)) for _ in range(episodes_per_update)]
+    initial_learning_rate = 1.0
+    learning_rate = initial_learning_rate
+    for learning_iteration in range(100):
         targets = list(TD0_targets(episodes, q))
+        targets = random.choices(targets, k=1000)
         X, Y_target = zip(*targets)
-        assert_shapetype(X, 'int64', (-1,-1))
-        assert_shapetype(Y_target, 'float64', (-1,1))
+        Y_target = np.reshape(Y_target, (-1, 1))
+
+        learning_rate = decay(initial_learning_rate, learning_iteration)
         q.learn(learning_rate, X, Y_target)
 
-        if learn_iteration % 1 == 0:
             greedy_policy = Policy_Greedy(q)
             reward_sum = test_policy(greedy_policy, env)
-            #n_trees = len(q.trees)
-            #print(f"Episode {learn_iteration} RewardSum {reward_sum} Ntrees {n_trees}".format(learn_iteration, reward_sum, n_trees))
-            print(f"Episode {learn_iteration} RewardSum {reward_sum} lr {learning_rate} epsilon {epsilon}".format(learn_iteration, reward_sum, learning_rate, epsilon))
+        print(f"Episode {learning_iteration} RewardSum {reward_sum} lr {learning_rate}")
+
 
 if __name__ == '__main__':
-    runner()
+    run()
