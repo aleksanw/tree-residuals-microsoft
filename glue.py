3c6a2ff85a781513b0c874a6097d426d374c771f
Aleksander Wasaznik <aleksander@wasaznik.com>
2017-11-27 16:29:00 +0100

    Gluify corridorSmall-v5

--- glue.py
+++ glue.py
@@ -15,16 +15,29 @@ from time import sleep
 from pprint import pprint
 
 
+def avg(it):
+    length = 0
+    sum_ = 0
+    for x in it:
+        length += 1
+        sum_ += x
+    return sum_/length
+
+
 def run(env):
     action_space = list(range(env.action_space.n))
     replay_buffer = Replay_buffer()
 
     q = Approximator_ResidualBoosting(action_space)
-    initial_learning_rate = 1.0
+    initial_learning_rate = 0.15
     learning_rate = initial_learning_rate
+    initial_epsilon = 0.15
+    epsilon = initial_epsilon
+    batch_size = 10
+
     for learning_iteration in range(100):
-        policy = Policy_EpsilonGreedy(q, epsilon=0.5)
-        episodes = [list(rollout(policy, env)) for _ in range(1)]
+        policy = Policy_EpsilonGreedy(q, epsilon=epsilon)
+        episodes = [list(rollout(policy, env)) for _ in range(batch_size)]
         replay_buffer += episodes
         sampled_episodes = replay_buffer.sample(50)
         targets = TD0_targets(sampled_episodes, q)
@@ -32,8 +45,10 @@ def run(env):
         Y_target = np.reshape(Y_target, (-1, 1))
 
         learning_rate = decay(initial_learning_rate, learning_iteration)
+        epsilon = decay(initial_epsilon, learning_iteration)
         q.learn(learning_rate, X, Y_target)
 
-        greedy_policy = Policy_Greedy(q)
-        reward_sum = test_policy(greedy_policy, env)
-        print(f"Episode {learning_iteration} RewardSum {reward_sum} lr {learning_rate}")
+        if learning_iteration % 1 == 0:
+            greedy_policy = Policy_Greedy(q)
+            reward_sum = avg(test_policy(greedy_policy, env) for _ in range(10))
+            print(f"Episode {learning_iteration*batch_size} Reward {reward_sum} lr {learning_rate} epsilon {epsilon}")
