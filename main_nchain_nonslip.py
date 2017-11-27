a62bed14b68d53d20c03a19aefbf31c3b48ad34c
Aleksander Wasaznik <aleksander@wasaznik.com>
2017-11-27 15:44:37 +0100

    Use replay_buffer in nchain

--- main_nchain_nonslip.py
+++ main_nchain_nonslip.py
@@ -10,6 +10,7 @@ from approximator import Approximator_Table, Approximator_ResidualBoosting
 from policy import Policy_ConstantActionLoop, Policy_EpsilonGreedy, Policy_Greedy, Policy_Deterministic
 from td import TD0_targets
 from utils import rollout,test_rollout, test_policy, decay, assert_shapetype, v
+from replay_buffer import Replay_buffer
 
 # Used under development
 import sys
@@ -21,14 +22,17 @@ def run():
     env = gym.make('NChain-v0')
     env.unwrapped.slip = 0  # nonslip env
     action_space = list(range(env.action_space.n))
+    replay_buffer = Replay_buffer()
 
     q = Approximator_ResidualBoosting(action_space)
     initial_learning_rate = 1.0
     learning_rate = initial_learning_rate
     for learning_iteration in range(100):
         policy = Policy_EpsilonGreedy(q, epsilon=0.5)
-        episodes = [rollout(policy, env) for _ in range(10)]
-        targets = TD0_targets(episodes, q)
+        episodes = [list(rollout(policy, env)) for _ in range(1)]
+        replay_buffer += episodes
+        sampled_episodes = replay_buffer.sample(50)
+        targets = TD0_targets(sampled_episodes, q)
         X, Y_target = zip(*targets)
         Y_target = np.reshape(Y_target, (-1, 1))
 
