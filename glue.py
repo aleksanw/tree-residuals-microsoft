e0a2c19b7f8c0bff5f4971e3102695a3c0a5284c
Aleksander Wasaznik <aleksander@wasaznik.com>
2017-12-04 13:02:26 +0100

    Add policy display to glue

--- glue.py
+++ glue.py
@@ -14,6 +14,7 @@ import sys
 from time import sleep
 from pprint import pprint
 
+import matplotlib.pyplot as plt
 
 def avg(it):
     length = 0
@@ -23,6 +24,48 @@ def avg(it):
         sum_ += x
     return sum_/length
 
+class PolicyPlotter:
+    def __init__(self):
+        plt.ion()
+        self.fig = plt.figure()
+        self.ax = self.fig.gca()
+
+    def update(self, env, policy):
+        X,Y,U,V = self.policy_to_xyuv(env, policy)
+        self.ax.clear()
+        self.ax.invert_yaxis()
+        self.ax.quiver(X,Y,U,V, angles='xy', pivot='mid')
+        self.fig.tight_layout()
+        self.fig.canvas.draw()
+
+    @staticmethod
+    def policy_to_xyuv(env, policy):
+        right = (1, 0)
+        left = (-1, 0)
+        up = (0, -1)
+        down = (0, 1)
+        action_to_uv = {0: left, 1: down, 2: right, 3: up}
+
+        X = []
+        Y = []
+        U = []
+        V = []
+
+        world = env.unwrapped.desc
+        world_height, world_width = world.shape
+
+        for y in range(0, world_height):
+            for x in range(0, world_width):
+                if world[y,x] == b'F':
+                    observation = [y*world_width + x]
+                    action = policy(observation)
+                    u, v = action_to_uv.get(action, (0,0))
+                    X.append(x)
+                    Y.append(y)
+                    U.append(u)
+                    V.append(v)
+
+        return X,Y,U,V
 
 def run(env):
     action_space = list(range(env.action_space.n))
@@ -31,13 +74,20 @@ def run(env):
     q = Approximator_ResidualBoosting(action_space)
     initial_learning_rate = 0.50
     learning_rate = initial_learning_rate
-    initial_epsilon = 0.30
+    initial_epsilon = 0.80
     epsilon = initial_epsilon
-    batch_size = 1
+    batch_size = 50
 
-    for learning_iteration in range(100):
+    interaction_count = 0
+    interactions = []
+    rewards = []
+
+    policy_plot = PolicyPlotter()
+
+    for learning_iteration in range(300):
         policy = Policy_EpsilonGreedy(q, epsilon=epsilon)
         episodes = [list(rollout(policy, env)) for _ in range(batch_size)]
+        interaction_count += sum(map(len, episodes))
         replay_buffer += episodes
         sampled_episodes = replay_buffer.sample(50)
         targets = TD0_targets(sampled_episodes, q)
@@ -50,5 +100,10 @@ def run(env):
 
         if learning_iteration % 1 == 0:
             greedy_policy = Policy_Greedy(q)
-            reward_sum = avg(test_policy(greedy_policy, env) for _ in range(10))
+            policy_plot.update(env, greedy_policy)
+            reward_sum = avg(test_policy(greedy_policy, env) for _ in range(1))
+            interactions.append(interaction_count)
+            rewards.append(reward_sum)
             print(f"Episode {learning_iteration*batch_size} Reward {reward_sum} lr {learning_rate} epsilon {epsilon}")
+
+    return interactions, rewards
