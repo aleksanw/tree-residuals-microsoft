07bef162a654acb8e72aa77a4e734d20229bc2f9
Olav Markussen <olavmarkussen@hotmail.no>
2018-01-16 20:00:13 +0100

    Move loading of pickle to helperfile

--- main_plot_envs_methods.py
+++ main_plot_envs_methods.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use('Agg')
 import os
 
 import corridor
@@ -7,9 +9,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 import glue as tree_agent
-import dqn as dqn_agent
 
 import pickle
+import helpers.pickler as pickler
+import helpers.plotter as plotter
 
 
 def nonslip_nchain():
@@ -39,33 +42,13 @@ def dropafter(predicate, iterable):
             return
 
 def main():
-    for make_env in envs:
-        if os.path.isfile('tree_run.pickle'):
-            tree_run_result = pickle.load(open('tree_run.pickle', 'rb'))
-        else:
-            tree_run_result = tree_agent.run(make_env())
-            tree_run_result = list(dropafter(lambda x: x[1] == 9960, tree_run_result))
-            pickle.dump(tree_run_result, open('tree_run.pickle', 'wb'))
+    basedir = 'perfs'
+    env_name = 'NChain-v0'
+    model = 'tree'
+    result = pickler.load(os.path.join(basedir, env_name, model))
+    print(result)
 
-        if os.path.isfile('dqn_run.pickle'):
-            dqn_run_result = pickle.load(open('dqn_run.pickle', 'rb'))
-        else:
-            dqn_run_result = dqn_agent.run(make_env())
-            dqn_run_result = list(dropafter(lambda x: x[1] == 9960, dqn_run_result))
-            pickle.dump(dqn_run_result, open('dqn_run.pickle', 'wb'))
-
-        both_results = pd.DataFrame({
-             'Tree': pd.Series(dict(tree_run_result)),
-             'DQN': pd.Series(dict(dqn_run_result)),
-             })
-
-        fig = plt.figure()
-        ax = fig.gca(xlabel='Interactions with environment',
-                     ylabel='Episode reward')
-        both_results.plot(ax=ax)
-        fig.tight_layout()
-        plt.show()
-
+    plotter.plot_with_mean(env_name, result)
 
 if __name__ == '__main__':
     main()
