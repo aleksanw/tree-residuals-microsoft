c0e3a5f0ec0218585f9ad8488b0c4c0411a46359
Olav Markussen <olavmarkussen@hotmail.no>
2018-01-16 19:52:34 +0100

    Plot a series of performances in one plot

--- helpers/plotter.py
+++ helpers/plotter.py
@@ -3,22 +3,25 @@ import pandas as pd
 
 figures = 'figures/'
 
-def plot(name, run_yields):
-    data = list(zip(*run_yields))
+def plot(name, perfs):
+    data = list(zip(*perfs))
 
     plt.plot(*data)
     plt.savefig(figures + name + '.pdf', format='pdf', dpi=1000)
 
-def plot_rolling_mean(env_name, run_yields):
-    data = pd.DataFrame({
-        'Tree': pd.Series(dict(run_yields)),
-    })
-    data['Moving Average'] = data.rolling(window=20, min_periods=1,
-            center=True).mean()
-
+def plot_with_mean(env_name, perfs):
     fig = plt.figure()
     ax = fig.gca(xlabel='Interactions with environment',
                  ylabel=f'Episode reward in {env_name}')
-    data.plot(ax=ax)
-    fig.tight_layout()
+
+    df = pd.DataFrame()
+    for i, perf in enumerate(perfs):
+        ser =  pd.Series(dict(perf))
+        df[i] = ser
+        ser.plot(ax=ax, color='gray')
+
+    mean = df.mean(axis=1)
+    mean_df = pd.DataFrame({'Average': mean})
+    mean_df.plot(ax=ax, color='orange')
     plt.savefig(figures + env_name + '.pdf', format='pdf', dpi=1000)
+
