fb9b5c67b03a10a0b6e8f05b0f49952e8ec32859
Olav Markussen <olavmarkussen@hotmail.no>
2018-01-17 19:48:53 +0100

    Run selected agents on certain environments

--- main_plot_envs_methods.py
+++ main_plot_envs_methods.py
@@ -1,24 +1,20 @@
 import matplotlib
 matplotlib.use('Agg')
-import os
-
 import corridor
 import gym
-
 import matplotlib.pyplot as plt
+import multiprocessing
+import os
 import pandas as pd
-
-import glue as tree_agent
-
 import pickle
+
+import dqn as dqn_agent
+import glue as tree_agent
 import helpers.pickler as pickler
 import helpers.plotter as plotter
+from helpers.variables import write_variables_to_latex
 
 
-def nonslip_nchain():
-    env = gym.make('NChain-v0')
-    env.unwrapped.slip = 0  # nonslip env
-    return env
 
 def nchain_microsoft():
     env = gym.make('NChain-v0')
@@ -27,12 +23,19 @@ def nchain_microsoft():
     return env
 
 
-envs = [
-    #('Nonslip nchain', nonslip_nchain),
-    #('Blackjack', lambda: gym.make('Blackjack-v0')),
-    nonslip_nchain,
-    #lambda: gym.make('CorridorBig-v10'),
-    ]
+def run_tree(env_name):
+    env = gym.make(env_name)
+    params, prefs = tree_agent.run(env)
+    return params, dict(prefs)
+
+
+def run_dqn(env_name):
+    env = gym.make(env_name)
+    if env_name == 'Blackjack-v0':
+        env.observation_space.shape = [3]
+    params, prefs = dqn_agent.run(env)
+    return params, dict(prefs)
+
 
 
 def dropafter(predicate, iterable):
@@ -41,14 +44,41 @@ def dropafter(predicate, iterable):
         if predicate(x):
             return
 
+
+envs = [
+    'Blackjack-v0',
+    'NChain-v0' ,
+    ]
+
+agents = [
+    ('tree', run_tree),
+    ('dqn', run_dqn)
+    ]
+
+
 def main():
     basedir = 'perfs'
-    env_name = 'NChain-v0'
-    model = 'tree'
-    result = pickler.load(os.path.join(basedir, env_name, model))
-    print(result)
 
-    plotter.plot_with_mean(env_name, result)
+    for env_name in envs:
+        for agent_name, agent in agents:
+            pool = multiprocessing.Pool()
+            threads = 30
+            if agent_name == 'dqn':
+                threads = 1
+            results = pool.map(agent, [env_name]*threads)
+            perfs = [result[1] for result in results]
+
+            variables = results[0][0]
+            wanted_written = ['initial_learning_rate', 'initial_epsilon',
+                              'rollout_batch_size', 'num_episodes',
+            ]
+            write_variables_to_latex(variables, wanted_written,
+                    env_name, agent_name)
+
+            pickler.dump(os.path.join('perfs', env_name, 'tree.pickle'), perfs)
+            plotter.plot_with_mean(env_name, perfs)
+
+
 
 if __name__ == '__main__':
     main()
