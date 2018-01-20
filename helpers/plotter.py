f62dcdb9d370cc4bbf89ccd26064604ed0a439c4
Olav Markussen <olavmarkussen@hotmail.no>
2018-01-20 17:06:26 +0100

    Append time to figure filename

--- helpers/plotter.py
+++ helpers/plotter.py
@@ -1,13 +1,28 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import time
+import os
 
-figures = 'figures/'
+
+directory = 'figures'
+
+def compose_path(*subnames):
+    subnames = list(subnames)
+    suffix = '.pdf'
+    subnames.append(time.strftime("%Y_%m_%d_%H_%M"))
+    filename = '_'.join(subnames)
+    filename += suffix
+    path = os.path.join(directory, filename)
+
+    return path
+
 
 def plot(name, perfs):
     data = list(zip(*perfs))
 
     plt.plot(*data)
     plt.savefig(figures + name + '.pdf', format='pdf', dpi=1000)
+
 
 def plot_with_mean(env_name, agent_name, perfs):
     fig = plt.figure()
@@ -23,5 +38,6 @@ def plot_with_mean(env_name, agent_name, perfs):
     mean = df.mean(axis=1)
     mean_df = pd.DataFrame({'Average': mean})
     mean_df.plot(ax=ax, color='orange')
-    plt.savefig(figures + env_name + agent_name + '.pdf', format='pdf', dpi=1000)
+    path = compose_path(env_name, agent_name)
+    plt.savefig(path, format='pdf', dpi=1000)
 
