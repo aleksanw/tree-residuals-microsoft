1cf0fb39ef82dd5857dc6c2bb32944e8101d86b7
Olav Markussen <olavmarkussen@hotmail.no>
2018-01-26 15:43:33 +0100

    Add size to replay_buffer

--- replay_buffer.py
+++ replay_buffer.py
@@ -4,6 +4,7 @@ import numpy as np
 class Replay_buffer:
     def __init__(self):
         self.buffer = []
+        self.length = 30000
 
     def __iadd__(self, collection):
         self.buffer += collection
@@ -13,7 +14,13 @@ class Replay_buffer:
         self.buffer.append(item)
 
     def sample(self, n):
+        if len(self.buffer) >= self.length:
+            self.cleanup_buffer()
         return random.choices(self.buffer, k=n)
 
     def sample_as_nd(self, n):
         return np.array(self.sample(n))
+
+    def cleanup_buffer(self):
+        self.buffer = self.buffer[int(self.length/5):]
+        #self.buffer = self.buffer
